1c15d5ed9e488b34b44a045415eff4a7d70b5837
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-06-06 17:52:56 +0200

    Migliorie nella response

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -11,8 +11,9 @@ import { Product } from "../models/Product";
 export async function get_all_products(req: Request, res: Response) {
   try {
     const products = await prisma.product.findMany();
-    res.status(200).send(products);
+    res.status(200).json(products);
   } catch (error) {
+    console.error("Errore durante il fetch dei dati:", error);
     return res.status(500).send("Errore durante il fetch dei dati");
   }
 }
@@ -35,7 +36,7 @@ export async function add_product(req: Request, res: Response) {
         stock: product.stock,
       },
     });
-    res.status(201).send("Prodotto creato con successo");
+    res.status(201).json({ messagge: "Prodotto creato con successo" });
   } catch (error) {
     return res.status(500).send("Errore durante la creazione del prodotto");
   }
